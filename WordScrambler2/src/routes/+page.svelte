f0641d9d0ff90bbec56546f74bdcb0f03ca613c5
ValHallen96 <spornjohn9@gmail.com>
2024-08-04 18:20:10 -0400

    initial commit

---
+++ WordScrambler2/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    let word = "guess";
+    let wordArray = word.split('');
+    let scrambled = [];
+    let answerLength: string[] = [];
+
+    for(let i = 0; i < word.length; i++) {
+        let next = Math.floor(Math.random() * wordArray.length);
+        scrambled.push(wordArray[next]);
+        wordArray.splice(next, 1);
+    }
+    let solve = scrambled.join('');
+
+    answerLength.length = scrambled.length;
+</script>
+
+<section>
+    <div>
+        <h1>Word Scrambler 2.0</h1>
+        <p>Directions: Descramble the word.</p>
+    </div>
+    <div>
+        <h1>{solve}</h1>
+    </div>
+    <div>
+        {#each answerLength as item}
+            <input bind:value={item} />
+        {/each}
+    </div>
+</section>
