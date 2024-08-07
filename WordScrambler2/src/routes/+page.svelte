d7370c9c7e325d9add308a8d10e2e5c15d1ae4a4
ValHallen96 <spornjohn9@gmail.com>
2024-08-06 21:40:20 -0400

    Started styling page.

--- WordScrambler2/src/routes/+page.svelte
+++ WordScrambler2/src/routes/+page.svelte
@@ -1,30 +1,49 @@
 <script lang="ts">
-    let word = "guess";
-    let wordArray = word.split('');
+	import { onMount } from "svelte";
+    import "../app.css";
+
+    let fileContent: string[] = [];
     let scrambled = [];
     let answerLength: string[] = [];
+    let solve = '';
 
-    for(let i = 0; i < word.length; i++) {
-        let next = Math.floor(Math.random() * wordArray.length);
-        scrambled.push(wordArray[next]);
-        wordArray.splice(next, 1);
-    }
-    let solve = scrambled.join('');
+    onMount(async () => {
+        try {
+            const response = await fetch('src/data/words.json');
+            if(!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            fileContent = await response.json();  
+        }
+        catch (error: any) {
+            console.error(error.message);
+        }
+        let random = Math.floor(Math.random() * fileContent.length);
+        let word = fileContent[random];
+        let wordArray = word.split('');
 
-    answerLength.length = scrambled.length;
+        for(let i = 0; i < word.length; i++) {
+            let next = Math.floor(Math.random() * wordArray.length);
+            scrambled.push(wordArray[next]);
+            wordArray.splice(next, 1);
+        }
+        solve = scrambled.join('');
+
+        answerLength.length = scrambled.length;
+    });
 </script>
 
-<section>
-    <div>
-        <h1>Word Scrambler 2.0</h1>
-        <p>Directions: Descramble the word.</p>
+<section class="container mx-auto text-center">
+    <div class="py-3">
+        <h1 class="text-7xl font-bold">Word Scrambler 2.0</h1>
+        <p class="text-3xl">Directions: Descramble the word.</p>
     </div>
-    <div>
-        <h1>{solve}</h1>
+    <div class="py-3">
+        <h1 class="text-5xl">{solve}</h1>
     </div>
-    <div>
+    <div class="flex flex-row flex-wrap justify-center gap-3">
         {#each answerLength as item}
-            <input bind:value={item} />
+            <input class="border-2 outline outline-1 w-14 h-20 shadow-lg" bind:value={item} maxlength="1" />
         {/each}
     </div>
 </section>
